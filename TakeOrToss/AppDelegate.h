ba737cb22900b23db4d044e736a01a10d6dabeaf
Noppanit Charassinvichai <Noppanit.c@gmail.com>
2015-02-17 00:00:21 -0500

    Initial Commit

---
+++ TakeOrToss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TakeOrToss
+//
+//  Created by Noppanit Charassinvichai on 2/17/15.
+//  Copyright (c) 2015 Noppanit Charassinvichai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TakeOrTossTests/TakeOrTossTests.m
@@ -0,0 +1,40 @@
+//
+//  TakeOrTossTests.m
+//  TakeOrTossTests
+//
+//  Created by Noppanit Charassinvichai on 2/17/15.
+//  Copyright (c) 2015 Noppanit Charassinvichai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TakeOrTossTests : XCTestCase
+
+@end
+
+@implementation TakeOrTossTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
